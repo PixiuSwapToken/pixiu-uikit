e40469acda52e70bef592851b8b8956997c2eb01
pixiuswap <83927033+pixiuswap@users.noreply.github.com>
2021-06-30 16:44:09 +0300

    Initial commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,186 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://pixiuswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://pixiuswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pixiuswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pixiuswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pixiuswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pixiuswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      // {
+      //   label: "Voting",
+      //   href: "https://voting.pancakeswap.finance",
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/pixiuswap/",
+      },
+      {
+        label: "Docs",
+        href: "https://pixiuswap.gitbook.io/pixiuswap-finance/",
+      },
+      {
+        label: "Blog",
+        href: "https://pixiuswap.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/pixiuswaptoken",
+      },
+      // {
+      //   label: "Bahasa Indonesia",
+      //   href: "https://t.me/PancakeSwapIndonesia",
+      // },
+      // {
+        // label: "中文",
+        // href: "https://t.me/pixiuswaptokenchinese",
+      // },
+      // {
+      //   label: "Tiếng Việt",
+      //   href: "https://t.me/PancakeSwapVN",
+      // },
+      // {
+        // label: "Italiano",
+        // href: "https://t.me/pixiuswaptokenitalian",
+      // },
+      // {
+        // label: "русский",
+        // href: "https://t.me/pixiuswaptokenrussian",
+      // },
+      // {
+        // label: "Türkiye",
+        // href: "https://t.me/pixiuswaptokenTurkey",
+      // },
+      // {
+        // label: "Português",
+        // href: "https://t.me/PancakeSwapPortuguese",
+      // },
+      // {
+        // label: "Español",
+        // href: "https://t.me/pixiuswaptokenspanish",
+      // },
+      // {
+        // label: "日本語",
+        // href: "https://t.me/pixiuswaptokenjapanese",
+      // },
+      // {
+        // label: "Français",
+        // href: "https://t.me/pixiuswaptoken",
+      // },
+      {
+        label: "Announcements",
+        href: "https://t.me/pixiuswapfinance",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/pixiuswaptoken",
+  },
+  {
+    label: "Reddit",
+    icon: "RedditIcon",
+    href: "https://www.reddit.com/r/pixiuswaptoken",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
